0a2c1790051b9fc2af88745038f58d6120b21d09
Alexandre Pedroza <alemkpedroza@outlook.com>
2023-09-18 21:16:43 -0300

    Update verificador_media.py

--- verificador_media.py
+++ verificador_media.py
@@ -1,20 +1,19 @@
-n1 = float(input("Entre com a nota 1: "))
-n2 = float(input("Entre com a nota 2: "))
-n3 = float(input("Entre com a nota 3: "))
-n4 = float(input("Entre com a nota 4: "))
-
-MA= (n1+n2+n3+n4)/4
-
-print("A sua nota final foi", MA)
-
-if MA <3:
-  print("Vc foi reprovado")
-
-else: 
-  if MA <=7:
-    print("Vc foi aprovado")
-
-  else:
-      print("AF")
-
-print("fim da media")
+while True:
+   NomeAluno = input("Entre com o nome do aluno: ")
+   n1 = float(input("Entre com a nota 1: "))
+   n2 = float(input("Entre com a nota 2: "))
+   n3 = float(input("Entre com a nota 3: "))
+   n4 = float(input("Entre com a nota 4: "))
+   MA= (n1+n2+n3+n4)/4
+   print("A sua nota final foi", MA)
+   if MA <=3:
+     print("Voce foi reprovado")
+   else:
+     MA >=7
+     print("Voce foi aprovado")
+     op=input("Mais aluno(s) (S/N)")
+     if op.upper()=='S':
+      continue
+     else:
+       break
+print("Fim da média")
